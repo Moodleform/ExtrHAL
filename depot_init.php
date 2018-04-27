17cc71a4e5a8eb17ef0acb411b3c10616046de73
Olivier Troccaz <olivier.troccaz@univ-rennes1.fr>
2018-04-27 10:04:00 +0200

    template

--- depot_init.php
+++ depot_init.php
@@ -4,7 +4,7 @@ mb_internal_encoding("UTF-8");
 ?>
 <body style='font-family: corbel'>
 <form enctype="multipart/form-data" action="depot_res.php" method="post" accept-charset="UTF-8">
-<b>Fichier dépôt (csv)</b> : <input name="depot" type="file" /><br/>
+<b>Fichier dépôt (csv)</b> (<a href="./template.csv">voir un exemple de template</a>) : <input name="depot" type="file" /><br/>
 Ajout de la version avec "director_t" : <input type="checkbox" name="dir_t" value="oui" /><br/>
 <input type="submit" value="Envoyer">
 </body>
